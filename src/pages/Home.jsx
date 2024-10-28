d83a3c2043e9a806a730d27ae94e3567fe45da30
arindom10 <karindom7@gmail.com>
2024-10-28 18:00:36 +0600

    home page added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,33 @@ import heroImg from "../assets/images/hero-img.png";
 
 const Home = () => {
   const year = new Date().getFullYear();
-  return <div>home</div>;
+  return (
+    <Helmet title={"Home"}>
+      <section className="hero__section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6">
+              <div className="hero__content">
+                <p className="hero__subtitle">Trending Products in {year}</p>
+                <h2>Make Your Interior More Minimalistic & Modern</h2>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  Mollitia voluptate totam sunt rerum assumenda blanditiis nobis
+                  neque facilis! Alias, sequi?
+                </p>
+                <button className="buy__button">SHOP NOW</button>
+              </div>
+            </Col>
+            <Col lg="6" md="6">
+              <div className="hero-img">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
 };
 
 export default Home;
