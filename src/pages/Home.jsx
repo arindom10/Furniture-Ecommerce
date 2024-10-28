e21c21eaa5c9146cdc9da2c0d96b96d59f16fff8
arindom10 <karindom7@gmail.com>
2024-10-28 18:00:10 +0600

    navbar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Helmet from "../components/Helmet/Helmet";
+
+import { Container, Row, Col } from "reactstrap";
+import heroImg from "../assets/images/hero-img.png";
 
 const Home = () => {
-  return <div>Home</div>;
+  const year = new Date().getFullYear();
+  return <div>home</div>;
 };
 
 export default Home;
