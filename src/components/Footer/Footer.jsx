9092746dc2d881e19a967e8b6b59fc1c77bcdbe1
arindom10 <karindom7@gmail.com>
2024-12-29 23:14:54 +0600

    add Footer component with quick links and contact information; implement sticky header functionality in Header component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,112 @@
 import React from "react";
+import "./footer.css";
+
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="4">
+            <div className="logo">
+              <div>
+                <Link className="text-white" to="/home">
+                  Multimart
+                </Link>
+              </div>
+            </div>
+            <p className="footer__text mt-3">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas sunt
+              vitae at ex ab perspiciatis ad neque nostrum pariatur omnis.
+            </p>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Top Categories</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Mobile Phones</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Modern Sofa</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Arm Chair</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Smart Watches</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="2">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Useful Links</h4>
+              <ListGroup>
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/shop">Shop</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/cart">Cart</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="/login">Login</Link>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0">
+                  <Link to="#">Privacy Policy</Link>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+
+          <Col lg="3">
+            <div className="footer__quick-links">
+              <h4 className="quick__links-title">Contact</h4>
+              <ListGroup className="footer__contact">
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-map-pin-line"></i>
+                  </span>
+                  <p>222/3, West Agargaon, Dhaka-1207</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-phone-line"></i>
+                  </span>
+                  <p>+8801735-049535</p>
+                </ListGroupItem>
+
+                <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i className="ri-mail-line"></i>
+                  </span>
+                  <p>medimart@gmail.com</p>
+                </ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col lg="12">
+            <p className="footer__copyright text-center">
+              CopyRight {year} developed by Arindom Kundu, All rights reserved.{" "}
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
